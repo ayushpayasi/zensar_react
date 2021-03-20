767f0b5f47cfd3e729147d3abbfb280ba09eed8e
avirao787073 <raoavnish787073@gmail.com>
2021-03-20 22:43:43 +0530

    created taskbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Taskbar from "./components/taskbar.component"
+import mouseclick from "./assets/mouseclick.mp3"
+import door from "./assets/door.mp3"
+import{BrowserRouter as Router , Route, Switch, Link} from "react-router-dom"
+import Home from "./components/home.component"
 function App() {
+  
+  const mouseclickAud = new Audio(mouseclick)
+  const doorAud = new Audio(door)
+  mouseclickAud.volume = 0.5
+  doorAud.volume = 0.5
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Taskbar mouseclick={mouseclickAud} door={doorAud}/>
+      <Switch>
+        <Route path="/" component={()=><Home/>}/>
+      </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/taskbar.component.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+import {Link} from "react-router-dom"
+import logo from "../assets/logo.png"
+
+export default function Taskbar(props) {
+
+    const clickHandler=(event)=>{
+        props.door.play()
+        const bun = document.getElementsByClassName("bun");
+        bun[0].style.transition = "transform 0.5s"
+        const bun_menu = document.getElementsByClassName("bun_menu")  
+        bun_menu[0].style.transition = "height 0.5s"
+        if(bun_menu[0].style.height === "25px"){
+            bun[0].style.transform = "rotate(0deg)";
+            bun_menu[0].style.height = "0px";
+    }
+        else{
+            bun[0].style.transform = "rotate(90deg)";
+            bun_menu[0].style.height = "25px";
+        }
+    
+    }
+
+    const playClick =(event)=>{
+        props.mouseclick.play()
+    }
+
+    return (
+        <React.Fragment>
+        <div className="taskbar_holder">
+            <div className="taskbar_home">
+              <Link onClick={(event)=>{playClick(event)}} className="links_anchor" to="/">  AyushPayasi</Link>
+            </div>
+            <div className="taskbar_content">
+                <div className="links">
+                    <img className="logo_img" src={logo}/>
+                </div>
+                <div className="links">
+                    <Link onClick={(event)=>{playClick(event)}} className="links_anchor" to="/about"><a> ABOUT</a></Link>
+                    <Link onClick={(event)=>{playClick(event)}} className="links_anchor" to="/contact"><a> CONTACT</a></Link>
+                    <Link onClick={(event)=>{playClick(event)}} className="links_anchor" to="/support"><a> SUPPORT</a> </Link>
+                </div>
+            </div>
+            <div className="bun">
+            <IconButton onClick={(event)=>{clickHandler(event)}}style={{color:"white"}} aria-label="menu">
+                <MenuIcon />
+            </IconButton>
+            </div>
+
+        </div>
+        <div className="bun_menu">
+        <div className="bun_menu_items">
+            <Link onClick={(event)=>{playClick(event)}} className="links_anchors" to="/about"><a> ABOUT</a></Link>
+            <Link onClick={(event)=>{playClick(event)}} className="links_anchors" to="/contact"><a> CONTACT</a></Link>
+            <Link onClick={(event)=>{playClick(event)}} className="links_anchors" to="/support"><a> SUPPORT</a> </Link>
+        </div>
+    </div></React.Fragment>
+    )
+}
+
